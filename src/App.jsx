8d6cfdf20e07e20c8fa4bc6e54e13a8a4151aaf5
yelamoste <108941678+yelamoste@users.noreply.github.com>
2025-04-02 19:50:37 +0800

    Changes for Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ function App() {
     <>
       <div>
         <div className="heartPicture"></div>
-        <p className="greetingsText" id="greetingsText1">
-          Happy
-        </p>
-        <p className="greetingsText" id="greetingsText2">
-          Birthday,
-        </p>
+        <div className="textContainer">
+          <p className="greetingsText" id="greetingsText1">
+            {/* Happy */}
+          </p>
+          <p className="greetingsText" id="greetingsText2">
+            {/* Birthday, */}
+          </p>
+        </div>
         <p className="heartName">Heart Ryan!</p>
       </div>
       <div className="confettiDesign" id="confettiLeft"></div>
