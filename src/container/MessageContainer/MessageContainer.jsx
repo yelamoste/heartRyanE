124df512c23cc0235073e3b576e5f57088632a16
yelamoste <108941678+yelamoste@users.noreply.github.com>
2025-04-09 21:54:49 +0800

    Latest Mobile Responsiveness

--- src/container/MessageContainer/MessageContainer.jsx
+++ src/container/MessageContainer/MessageContainer.jsx
@@ -38,7 +38,8 @@ export function MessageContainerLeft() {
           image: "/heartryanefc.jpg",
           time: "10:00 PM",
           name: "HeartBeatsOFCalsdaksjdlaksjdlasjdkasjdlakjsdlaksjdlkajsldkajsdlkajldkj",
-          content: "hello",
+          content:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         }}
       />
       <Messages
