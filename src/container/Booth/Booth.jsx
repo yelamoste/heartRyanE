7067f8f1a6ab01d7c499aec9de1252acb9083018
yelamoste <108941678+yelamoste@users.noreply.github.com>
2025-04-13 05:14:44 +0800

    WebCamera for Photobooth

--- src/container/Booth/Booth.jsx
+++ src/container/Booth/Booth.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Booth.css";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 1080,
+  height: 1080,
+  aspectRatio: 1,
+  facingMode: "user",
+};
+
 function Booth() {
-  return <div>booth</div>;
+  const [captured, setCaptured] = useState("");
+  const webcamRef = React.useRef(null);
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setCaptured(imageSrc);
+    console.log(imageSrc, "imageSrc");
+  }, [webcamRef]);
+
+  return (
+    <>
+      <div className="boothContainer">
+        <div className="frameContainer"></div>
+        <div className="cameraContainer">
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+            screenshotQuality={1}
+            mirrored={true}
+          />
+
+          <button onClick={capture}>Capture photo</button>
+        </div>
+        <img src={captured} id="imgCaptured" />
+      </div>
+    </>
+  );
 }
 
 export default Booth;
